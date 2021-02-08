bc383a8c63d8a76765077773e34137649e247276
Forrest Koch <forrest.c.koch@gmail.com>
2021-02-08 17:33:22 +1100

    checkpoing with diffeqpy before deletion

--- src/models.py
+++ src/models.py
@@ -1,14 +1,62 @@
 import math
 import itertools as it
 
+from numba import njit
 import numpy as np
 import scipy as sc
 import scipy.integrate as scint 
 import scipy.stats as scstats
 
+from diffeqpy import de
+
 def get_transmission_matrix(beta,X,Y):
     Z = np.array(list(it.product(X.flatten(),Y.flatten()))).prod(axis=1)
     return beta * Z.reshape(np.repeat(len(X)*len(Y),2))
+
+@njit
+def ode_func(V, trans_matrix, alpha, eta, gamma, n_groups):
+    N = V.sum(axis=0)
+    dV = np.zeros(V.shape)
+
+    s_end = n_groups
+    e_end = 2 * s_end
+    i_end = s_end + e_end # 3 * self.n_groups, but more efficient...
+
+    exposed = V[0:s_end] * ((trans_matrix @ (alpha * V[s_end:e_end] + V[e_end:i_end]))/N)
+    infectious = eta * V[s_end:e_end]
+    recovered = gamma * V[e_end:i_end]
+
+    dV[0:s_end] = -1 * exposed
+    dV[s_end:e_end] = exposed - infectious
+    dV[e_end:i_end] = infectious - recovered
+    dV[i_end:] = recovered
+    return dV
+
+@njit
+def ode_func2(V,p,t):
+    N = V.sum(axis=0)
+    dV = np.zeros(V.shape)
+
+    alpha = p[0]
+    eta = p[1]
+    gamma = p[2]
+    n_groups = p[3]
+    trans_matrix = p[4:].reshape((n_groups,n_groups))
+
+    s_end = n_groups
+    e_end = 2 * s_end
+    i_end = s_end + e_end 
+
+    exposed = V[0:s_end] * ((trans_matrix @ (alpha * V[s_end:e_end] + V[e_end:i_end]))/N)
+    infectious = eta * V[s_end:e_end]
+    recovered = gamma * V[e_end:i_end]
+
+    dV[0:s_end] = -1 * exposed
+    dV[s_end:e_end] = exposed - infectious
+    dV[e_end:i_end] = infectious - recovered
+    dV[i_end:] = recovered
+
+    return dV
 
 class StratifiedSEIR(object):
 
@@ -33,6 +81,7 @@ class StratifiedSEIR(object):
 
     def _check_params(self):
         pass
+
 
     def odes(self, t, V):
         """
@@ -62,6 +111,10 @@ class StratifiedSEIR(object):
 
         return dV
 
+    def odes2(self, t, V):
+        return ode_func(V, self.trans_matrix, self.alpha, self.eta, self.gamma, self.n_groups)
+
+
     def solve(self,method='RK45'):
         """
         Return the solution provided by scipy.integrate.solve_ivp
@@ -71,6 +124,25 @@ class StratifiedSEIR(object):
         V0 = np.array(list(it.product([self.S0,self.E0,self.I0,self.R0],self.group_ratios))).prod(axis=1)
 
         return scint.solve_ivp(fun=self.odes, t_span=self.t_span, y0=V0, t_eval=self.t_eval,vectorized=True,method=method)
+
+    def solve2(self,method='RK45'):
+        """
+        Return the solution provided by scipy.integrate.solve_ivp
+        """
+        # Note the order of the arguments is important here
+        #print(self.group_ratios)
+        V0 = np.array(list(it.product([self.S0,self.E0,self.I0,self.R0],self.group_ratios))).prod(axis=1)
+
+        return scint.solve_ivp(fun=self.odes2, t_span=self.t_span, y0=V0, t_eval=self.t_eval,vectorized=True,method=method)
+
+    def solve3(self):
+        #V0 = np.array(list(it.product([self.S0,self.E0,self.I0,self.R0],self.group_ratios))).prod(axis=1)
+        V0 = np.array(list(it.product([self.S0,self.E0,self.I0,self.R0],[.5,0.5]))).prod(axis=1)
+
+        #prob = de.ODEProblem(ode_func2, V0, self.t_span, np.concatenate([[self.alpha,self.eta,self.gamma,self.n_groups],self.trans_matrix.flatten()]))
+        prob = de.ODEProblem(ode_func2, list(V0), (0,100), [self.alpha,self.eta,self.gamma,2,1,1,1,1])
+        return de.solve(prob)
+
 
     def copy(self):
         return None
